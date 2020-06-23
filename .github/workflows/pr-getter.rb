474c197955f1a5df90e38fa8fe4794245501a921
thinceller <thinceller@gmail.com>
2020-06-23 19:36:31 +0900

    Fix

--- .github/workflows/pr-getter.rb
+++ .github/workflows/pr-getter.rb
@@ -2,24 +2,28 @@
 
 require 'net/http'
 require 'uri'
+require 'json'
 
 def main
   github_token = ENV.fetch('TOKEN')
-  url = ENV.fetch('PR_URL', { token: github_token })
+  url = ENV.fetch('PR_URL')
 
-  res = fetch(url)
+  res = fetch(url, { token: github_token })
   puts res['head']
 end
 
 def fetch(url, option)
   uri = URI.parse(url)
 
-  req = Net::HTTP::Get.new(url.path)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = uri.scheme == 'https'
+
+  req = Net::HTTP::Get.new(uri.path)
   req['Content-type'] = 'application/json'
   req['authorization'] = "Bearer #{option[:token]}"
 
-  res = Net::HTTP.start(url.host, url.port) do |http|
-    http.request(req)
+  res = http.start do |h|
+    h.request(req)
   end
 
   JSON.parse(res.body)
